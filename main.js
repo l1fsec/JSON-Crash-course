8c8fe41bf82030aebf801d1eba7967a29ba145dd
EmberNya <54239712+l1fsec@users.noreply.github.com>
2021-06-27 11:21:44 +0200

    Start

---
+++ main.js
@@ -0,0 +1,18 @@
+var person = {
+name: "Bob",
+age: 35,
+address:{
+  street: "5 main st",
+  city: "Boston"
+  },
+  children:["Brianna", "Nicolas"]
+}
+
+// person = JSON.stringify(person);
+// person = JSON.parse(person);
+
+//console.log(person.name);
+
+var people = [
+  
+]
